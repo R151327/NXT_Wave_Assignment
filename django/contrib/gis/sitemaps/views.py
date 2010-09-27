R151327/NXT_Wave_Assignment

commit ad10970de2c7e5d74843594b3ed74c1de2aed146

Fixed #13827 -- Cleaned up a few unnecessary function calls.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@13876 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/contrib/gis/sitemaps/views.py
@@ -93,7 +93,7 @@ def kml(request, label, model, field_name=None, compress=False, using=DEFAULT_DB
         else:
             qs = klass._default_manager.using(using).all()
         for mod in qs:
-            setattr(mod, 'kml', getattr(mod, field_name).kml)
+            mod.kml = getattr(mod, field_name).kml)
             placemarks.append(mod)
 
     # Getting the render function and rendering to the correct.

django/conf/__init__.py
@@ -102,7 +102,7 @@ class Settings(object):
                 new_installed_apps.append(app)
         self.INSTALLED_APPS = new_installed_apps
 
-        if hasattr(time, 'tzset') and getattr(self, 'TIME_ZONE'):
+        if hasattr(time, 'tzset') and self.TIME_ZONE:
             # When we can, attempt to validate the timezone. If we can't find
             # this file, no check happens and it's harmless.
             zoneinfo_root = '/usr/share/zoneinfo'

django/contrib/admin/validation.py
@@ -170,7 +170,7 @@ def validate_inline(cls, parent, parent_model):
     fk = _get_foreign_key(parent_model, cls.model, fk_name=cls.fk_name, can_fail=True)
 
     # extra = 3
-    if not isinstance(getattr(cls, 'extra'), int):
+    if not isinstance(cls.extra, int):
         raise ImproperlyConfigured("'%s.extra' should be a integer."
                 % cls.__name__)
 

django/contrib/auth/__init__.py
@@ -20,15 +20,12 @@ def load_backend(path):
         cls = getattr(mod, attr)
     except AttributeError:
         raise ImproperlyConfigured('Module "%s" does not define a "%s" authentication backend' % (module, attr))
-    try:
-        getattr(cls, 'supports_object_permissions')
-    except AttributeError:
+    if not hasattr(cls, "supports_object_permissions"):
         warn("Authentication backends without a `supports_object_permissions` attribute are deprecated. Please define it in %s." % cls,
              PendingDeprecationWarning)
         cls.supports_object_permissions = False
-    try:
-        getattr(cls, 'supports_anonymous_user')
-    except AttributeError:
+
+    if not hasattr(cls, 'supports_anonymous_user'):
         warn("Authentication backends without a `supports_anonymous_user` attribute are deprecated. Please define it in %s." % cls,
              PendingDeprecationWarning)
         cls.supports_anonymous_user = False

django/core/cache/__init__.py
@@ -63,7 +63,7 @@ def get_cache(backend_uri):
     else:
         name = scheme
     module = importlib.import_module(name)
-    return getattr(module, 'CacheClass')(host, params)
+    return module.CacheClass(host, params)
 
 cache = get_cache(settings.CACHE_BACKEND)
 

django/db/models/base.py
@@ -508,7 +508,7 @@ class Model(object):
                     # autopopulate the _order field
                     field = meta.order_with_respect_to
                     order_value = manager.using(using).filter(**{field.name: getattr(self, field.attname)}).count()
-                    setattr(self, '_order', order_value)
+                    self._order = order_value
 
                 if not pk_set:
                     if force_update:

django/db/models/options.py
@@ -79,14 +79,14 @@ class Options(object):
             # unique_together can be either a tuple of tuples, or a single
             # tuple of two strings. Normalize it to a tuple of tuples, so that
             # calling code can uniformly expect that.
-            ut = meta_attrs.pop('unique_together', getattr(self, 'unique_together'))
+            ut = meta_attrs.pop('unique_together', self.unique_together)
             if ut and not isinstance(ut[0], (tuple, list)):
                 ut = (ut,)
-            setattr(self, 'unique_together', ut)
+            self.unique_together = ut
 
             # verbose_name_plural is a special case because it uses a 's'
             # by default.
-            setattr(self, 'verbose_name_plural', meta_attrs.pop('verbose_name_plural', string_concat(self.verbose_name, 's')))
+            self.verbose_name_plural = meta_attrs.pop('verbose_name_plural', string_concat(self.verbose_name, 's'))
 
             # Any leftover attributes must be invalid.
             if meta_attrs != {}:

django/db/models/query_utils.py
@@ -257,9 +257,8 @@ def deferred_class_factory(model, attrs):
     deferred attributes to a particular instance of the model.
     """
     class Meta:
-        pass
-    setattr(Meta, "proxy", True)
-    setattr(Meta, "app_label", model._meta.app_label)
+        proxy = True
+        app_label = model._meta.app_label
 
     # The app_cache wants a unique name for each model, otherwise the new class
     # won't be created (we get an old one back). Therefore, we generate the

commit 090aca642da36b2a65fdbbadab374f0eee6bfc66

Fixed a geographic sitemaps regression introduced in r13876.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@13879 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/contrib/gis/sitemaps/views.py
@@ -93,7 +93,7 @@ def kml(request, label, model, field_name=None, compress=False, using=DEFAULT_DB
         else:
             qs = klass._default_manager.using(using).all()
         for mod in qs:
-            mod.kml = getattr(mod, field_name).kml)
+            mod.kml = getattr(mod, field_name).kml
             placemarks.append(mod)
 
     # Getting the render function and rendering to the correct.