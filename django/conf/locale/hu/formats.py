R151327/NXT_Wave_Assignment

commit 557945d1c706f2c83d7f798effb74b11c6afe745

Fixed #16076 -- Updated Hungarian l10n formats. Thanks, slink.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@16429 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/conf/locale/hu/formats.py
@@ -5,19 +5,28 @@
 # The *_FORMAT strings use the Django date format syntax,
 # see http://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
 DATE_FORMAT = 'Y. F j.'
-TIME_FORMAT = 'G:i:s'
-# DATETIME_FORMAT = 
-# YEAR_MONTH_FORMAT = 
+TIME_FORMAT = 'G.i.s'
+DATETIME_FORMAT = 'Y. F j. G.i.s'
+YEAR_MONTH_FORMAT = 'Y. F'
 MONTH_DAY_FORMAT = 'F j.'
 SHORT_DATE_FORMAT = 'Y.m.d.'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
+SHORT_DATETIME_FORMAT = 'Y.m.d. G.i.s'
+FIRST_DAY_OF_WEEK = 1 # Monday
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
+DATE_INPUT_FORMATS = (
+    '%Y.%m.%d.', # '2006.10.25.'
+)
+TIME_INPUT_FORMATS = (
+    '%H.%M.%S', # '14.30.59'
+    '%H.%M',    # '14.30'
+)
+DATETIME_INPUT_FORMATS = (
+    '%Y.%m.%d. %H.%m.%S',   # '2006.10.25. 14.30.59'
+    '%Y.%m.%d. %H.%M',      # '2006.10.25. 14.30'
+    '%Y.%m.%d.',            # '2006.10.25.'
+)
 DECIMAL_SEPARATOR = ','
 THOUSAND_SEPARATOR = u' ' # Non-breaking space
-# NUMBER_GROUPING = 
+NUMBER_GROUPING = 3

commit 1a9391ccd7d0059232a262f8ee84053024cb010e

Fixed #16520 -- Fixed typo in Hungarian localization formats introduced in r16429. Thanks, slink.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@16558 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/conf/locale/hu/formats.py
@@ -23,7 +23,7 @@ TIME_INPUT_FORMATS = (
     '%H.%M',    # '14.30'
 )
 DATETIME_INPUT_FORMATS = (
-    '%Y.%m.%d. %H.%m.%S',   # '2006.10.25. 14.30.59'
+    '%Y.%m.%d. %H.%M.%S',   # '2006.10.25. 14.30.59'
     '%Y.%m.%d. %H.%M',      # '2006.10.25. 14.30'
     '%Y.%m.%d.',            # '2006.10.25.'
 )