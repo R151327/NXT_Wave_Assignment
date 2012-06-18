django/django

Fixed #18444 -- Replace hard coded "View on Site" URLs

django/contrib/admin/sites.py
@@ -231,7 +231,8 @@ class AdminSite(object):
                 wrap(self.i18n_javascript, cacheable=True),
                 name='jsi18n'),
             url(r'^r/(?P<content_type_id>\d+)/(?P<object_id>.+)/$',
-                wrap(contenttype_views.shortcut)),
+                wrap(contenttype_views.shortcut),
+                name='shortcut'),
             url(r'^(?P<app_label>\w+)/$',
                 wrap(self.app_index),
                 name='app_list')

R151327/NXT_Wave_Assignment

django/contrib/admin/sites.py
@@ -231,7 +231,8 @@ class AdminSite(object):
                 wrap(self.i18n_javascript, cacheable=True),
                 name='jsi18n'),
             url(r'^r/(?P<content_type_id>\d+)/(?P<object_id>.+)/$',
-                wrap(contenttype_views.shortcut)),
+                wrap(contenttype_views.shortcut),
+                name='view_on_site'),
             url(r'^(?P<app_label>\w+)/$',
                 wrap(self.app_index),
                 name='app_list')