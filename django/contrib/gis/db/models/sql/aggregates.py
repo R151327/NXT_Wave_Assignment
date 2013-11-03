django/django

commit 36ded01527b690b5df0574492af9cfcc2ea3d1dc

Fixed #21302 -- Fixed unused imports and import *.

django/contrib/gis/db/models/sql/aggregates.py
@@ -1,4 +1,4 @@
-from django.db.models.sql.aggregates import *
+from django.db.models.sql.aggregates import Aggregate
 from django.contrib.gis.db.models.fields import GeometryField
 
 

django/contrib/gis/db/models/aggregates.py
@@ -1,5 +1,7 @@
 from django.db.models import Aggregate
 
+__all__ = ['Collect', 'Extent', 'Extent3D', 'MakeLine', 'Union']
+
 
 class Collect(Aggregate):
     name = 'Collect'

django/db/models/aggregates.py
@@ -3,6 +3,10 @@ Classes to represent the definitions of aggregate functions.
 """
 from django.db.models.constants import LOOKUP_SEP
 
+__all__ = [
+    'Aggregate', 'Avg', 'Count', 'Max', 'Min', 'StdDev', 'Sum', 'Variance',
+]
+
 
 def refs_aggregate(lookup_parts, aggregates):
     """

R151327/NXT_Wave_Assignment

commit 6a8074597c77ac4755af46bf532bc05c5d1551e6

Fixed regressions from 36ded01527b690b5df0574492af9cfcc2ea3d1dc.
Refs #21302

django/contrib/gis/db/models/sql/aggregates.py
@@ -1,8 +1,9 @@
-from django.db.models.sql.aggregates import Aggregate, Count
+from django.db.models.sql import aggregates
+from django.db.models.sql.aggregates import *  # NOQA
 from django.contrib.gis.db.models.fields import GeometryField
 
 
-__all__ = ['Count', 'Collect', 'Extent', 'Extent3D', 'MakeLine', 'Union']
+__all__ = ['Collect', 'Extent', 'Extent3D', 'MakeLine', 'Union'] + aggregates.__all__
 
 
 class GeoAggregate(Aggregate):

django/db/models/sql/aggregates.py
@@ -5,6 +5,10 @@ import copy
 
 from django.db.models.fields import IntegerField, FloatField
 
+
+__all__ = ['Aggregate', 'Avg', 'Count', 'Max', 'Min', 'StdDev', 'Sum', 'Variance']
+
+
 # Fake fields used to identify aggregate types in data-conversion operations.
 ordinal_aggregate_field = IntegerField()
 computed_aggregate_field = FloatField()