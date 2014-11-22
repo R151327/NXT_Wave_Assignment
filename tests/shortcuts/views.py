django/django

Avoided rewrapping Contexts in render_to_response.
This change preserves backwards-compatibility for a very common misuse
of render_to_response which even occurred in the official documentation.

It fixes that misuse wherever it happened in the code base and docs.

Context.__init__ is documented as accepting a dict and nothing else.
Since Context is dict-like, Context(Context({})) could work to some
extent. However, things get complicated with RequestContext and that
gets in the way of refactoring the template engine. This is the real
rationale for this change.

tests/shortcuts/views.py
@@ -1,0 +1,12 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def context_processor(request):
+    return {'output': 'context processor output'}
+
+
+def request_context_misuse(request):
+    context_instance = RequestContext(request, {}, processors=[context_processor])
+    # Incorrect -- context_instance should be passed as a keyword argument.
+    return render_to_response('shortcuts/output.html', context_instance)

R151327/NXT_Wave_Assignment

tests/shortcuts/views.py
@@ -33,6 +33,15 @@ def render_to_response_view_with_dirs(request):
     return render_to_response('render_dirs_test.html', dirs=dirs)
 
 
+def context_processor(request):
+    return {'bar': 'context processor output'}
+
+
+def render_to_response_with_context_instance_misuse(request):
+    context_instance = RequestContext(request, {}, processors=[context_processor])
+    # Incorrect -- context_instance should be passed as a keyword argument.
+    return render_to_response('shortcuts/render_test.html', context_instance)
+
 def render_view(request):
     return render(request, 'shortcuts/render_test.html', {
         'foo': 'FOO',