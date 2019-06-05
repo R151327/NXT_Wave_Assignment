django/django

commit c690afb873cac8035a3cb3be7c597a5ff0e4b261

Refs #28643 -- Changed Avg() to use NumericOutputFieldMixin.
Keeps precision instead of forcing DecimalField to FloatField.

django/db/models/functions/mixins.py
@@ -1,6 +1,6 @@
 import sys
 
-from django.db.models.fields import DecimalField, FloatField
+from django.db.models.fields import DecimalField, FloatField, IntegerField
 from django.db.models.functions import Cast
 
 
@@ -23,5 +23,9 @@ class FixDecimalInputMixin:
 class NumericOutputFieldMixin:
 
     def _resolve_output_field(self):
-        has_decimals = any(isinstance(s.output_field, DecimalField) for s in self.get_source_expressions())
-        return DecimalField() if has_decimals else FloatField()
+        source_expressions = self.get_source_expressions()
+        if any(isinstance(s.output_field, DecimalField) for s in source_expressions):
+            return DecimalField()
+        if any(isinstance(s.output_field, IntegerField) for s in source_expressions):
+            return FloatField()
+        return super()._resolve_output_field() if source_expressions else FloatField()

django/db/models/aggregates.py
@@ -3,7 +3,8 @@ Classes to represent the definitions of aggregate functions.
 """
 from django.core.exceptions import FieldError
 from django.db.models.expressions import Case, Func, Star, When
-from django.db.models.fields import DecimalField, FloatField, IntegerField
+from django.db.models.fields import FloatField, IntegerField
+from django.db.models.functions.mixins import NumericOutputFieldMixin
 
 __all__ = [
     'Aggregate', 'Avg', 'Count', 'Max', 'Min', 'StdDev', 'Sum', 'Variance',
@@ -93,15 +94,9 @@ class Aggregate(Func):
         return options
 
 
-class Avg(Aggregate):
+class Avg(NumericOutputFieldMixin, Aggregate):
     function = 'AVG'
     name = 'Avg'
-
-    def _resolve_output_field(self):
-        source_field = self.get_source_fields()[0]
-        if isinstance(source_field, (IntegerField, DecimalField)):
-            return FloatField()
-        return super()._resolve_output_field()
 
     def as_mysql(self, compiler, connection, **extra_context):
         sql, params = super().as_sql(compiler, connection, **extra_context)

tests/aggregation/tests.py
@@ -865,15 +865,15 @@ class AggregateTestCase(TestCase):
 
     def test_avg_decimal_field(self):
         v = Book.objects.filter(rating=4).aggregate(avg_price=(Avg('price')))['avg_price']
-        self.assertIsInstance(v, float)
-        self.assertEqual(v, Approximate(47.39, places=2))
+        self.assertIsInstance(v, Decimal)
+        self.assertEqual(v, Approximate(Decimal('47.39'), places=2))
 
     def test_order_of_precedence(self):
         p1 = Book.objects.filter(rating=4).aggregate(avg_price=(Avg('price') + 2) * 3)
-        self.assertEqual(p1, {'avg_price': Approximate(148.18, places=2)})
+        self.assertEqual(p1, {'avg_price': Approximate(Decimal('148.18'), places=2)})
 
         p2 = Book.objects.filter(rating=4).aggregate(avg_price=Avg('price') + 2 * 3)
-        self.assertEqual(p2, {'avg_price': Approximate(53.39, places=2)})
+        self.assertEqual(p2, {'avg_price': Approximate(Decimal('53.39'), places=2)})
 
     def test_combine_different_types(self):
         msg = 'Expression contains mixed types. You must set output_field.'
@@ -1087,7 +1087,7 @@ class AggregateTestCase(TestCase):
                 return super().as_sql(compiler, connection, function='MAX', **extra_context)
 
         qs = Publisher.objects.annotate(
-            price_or_median=Greatest(Avg('book__rating'), Avg('book__price'))
+            price_or_median=Greatest(Avg('book__rating', output_field=DecimalField()), Avg('book__price'))
         ).filter(price_or_median__gte=F('num_awards')).order_by('num_awards')
         self.assertQuerysetEqual(
             qs, [1, 3, 7, 9], lambda v: v.num_awards)

tests/aggregation_regress/tests.py
@@ -401,7 +401,7 @@ class AggregationTests(TestCase):
                 When(pages__lt=400, then='discount_price'),
                 output_field=DecimalField()
             )))['test'],
-            22.27, places=2
+            Decimal('22.27'), places=2
         )
 
     def test_distinct_conditional_aggregate(self):
@@ -1041,7 +1041,7 @@ class AggregationTests(TestCase):
         books = Book.objects.values_list("publisher__name").annotate(
             Count("id"), Avg("price"), Avg("authors__age"), avg_pgs=Avg("pages")
         ).order_by("-publisher__name")
-        self.assertEqual(books[0], ('Sams', 1, 23.09, 45.0, 528.0))
+        self.assertEqual(books[0], ('Sams', 1, Decimal('23.09'), 45.0, 528.0))
 
     def test_annotation_disjunction(self):
         qs = Book.objects.annotate(n_authors=Count("authors")).filter(

R151327/NXT_Wave_Assignment

commit 2883f5d88dd568ca9c1815d391daf2e27fe03187

Fixed #30542 -- Fixed crash of numerical aggregations with filter.
Filters in annotations crashed when used with numerical-type
aggregations (i.e. Avg, StdDev, and Variance). This was caused as the
source expressions no not necessarily have an output_field (such as the
filter field), which lead to an AttributeError: 'WhereNode' object has
no attribute output_field.

Thanks to Chuan-Zheng Lee for the report.

Regression in c690afb873cac8035a3cb3be7c597a5ff0e4b261 and two following
commits.

django/db/models/functions/mixins.py
@@ -42,9 +42,9 @@ class FixDurationInputMixin:
 class NumericOutputFieldMixin:
 
     def _resolve_output_field(self):
-        source_expressions = self.get_source_expressions()
-        if any(isinstance(s.output_field, DecimalField) for s in source_expressions):
+        source_fields = self.get_source_fields()
+        if any(isinstance(s, DecimalField) for s in source_fields):
             return DecimalField()
-        if any(isinstance(s.output_field, IntegerField) for s in source_expressions):
+        if any(isinstance(s, IntegerField) for s in source_fields):
             return FloatField()
-        return super()._resolve_output_field() if source_expressions else FloatField()
+        return super()._resolve_output_field() if source_fields else FloatField()

tests/aggregation/test_filter_argument.py
@@ -1,8 +1,11 @@
 import datetime
 from decimal import Decimal
 
-from django.db.models import Case, Count, F, OuterRef, Q, Subquery, Sum, When
+from django.db.models import (
+    Avg, Case, Count, F, OuterRef, Q, StdDev, Subquery, Sum, Variance, When,
+)
 from django.test import TestCase
+from django.test.utils import Approximate
 
 from .models import Author, Book, Publisher
 
@@ -39,6 +42,16 @@ class FilteredAggregateTests(TestCase):
     def test_filtered_aggregates(self):
         agg = Sum('age', filter=Q(name__startswith='test'))
         self.assertEqual(Author.objects.aggregate(age=agg)['age'], 200)
+
+    def test_filtered_numerical_aggregates(self):
+        for aggregate, expected_result in (
+            (Avg, Approximate(66.7, 1)),
+            (StdDev, Approximate(24.9, 1)),
+            (Variance, Approximate(622.2, 1)),
+        ):
+            with self.subTest(aggregate=aggregate.__name__):
+                agg = aggregate('age', filter=Q(name__startswith='test'))
+                self.assertEqual(Author.objects.aggregate(age=agg)['age'], expected_result)
 
     def test_double_filtered_aggregates(self):
         agg = Sum('age', filter=Q(Q(name='test2') & ~Q(name='test')))